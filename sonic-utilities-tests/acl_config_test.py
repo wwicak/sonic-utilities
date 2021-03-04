258d343d7267c3354850fe2ed583a3d1a6f9af40
Danny Allen <daall@microsoft.com>
2021-03-04 10:47:59 -0800

    [201911][acl] Expand VLAN into VLAN members when creating an ACL table (#1477)
    Signed-off-by: Danny Allen <daall@microsoft.com>

---
+++ sonic-utilities-tests/acl_config_test.py
@@ -0,0 +1,82 @@
+import pytest
+
+import config.main as config
+
+from click.testing import CliRunner
+
+
+class TestConfigAcl(object):
+    def test_expand_vlan(self):
+        assert set(config.expand_vlan_ports("Vlan1000")) == {"Ethernet20", "Ethernet100"}
+
+    def test_expand_lag(self):
+        assert set(config.expand_vlan_ports("PortChannel1001")) == {"PortChannel1001"}
+
+    def test_expand_physical_interface(self):
+        assert set(config.expand_vlan_ports("Ethernet20")) == {"Ethernet20"}
+
+    def test_expand_empty_vlan(self):
+        with pytest.raises(ValueError):
+            config.expand_vlan_ports("Vlan3000")
+
+    def test_parse_table_with_vlan_expansion(self):
+        table_info = config.parse_acl_table_info("TEST", "L3", None, "Vlan1000", "egress")
+        assert table_info["type"] == "L3"
+        assert table_info["policy_desc"] == "TEST"
+        assert table_info["stage"] == "egress"
+
+        port_list = table_info["ports@"].split(",")
+        assert set(port_list) == {"Ethernet20", "Ethernet100"}
+
+    def test_parse_table_with_vlan_and_duplicates(self):
+        table_info = config.parse_acl_table_info("TEST", "L3", None, "Ethernet20,Vlan1000", "egress")
+        assert table_info["type"] == "L3"
+        assert table_info["policy_desc"] == "TEST"
+        assert table_info["stage"] == "egress"
+
+        # Since Ethernet4 is a member of Vlan1000 we should not include it twice in the output
+        port_list = table_info["ports@"].split(",")
+        assert len(port_list) == 2
+        assert set(port_list) == {"Ethernet20", "Ethernet100"}
+
+    def test_parse_table_with_empty_vlan(self):
+        with pytest.raises(ValueError):
+            config.parse_acl_table_info("TEST", "L3", None, "Ethernet20,Vlan3000", "egress")
+
+    def test_parse_table_with_invalid_ports(self):
+        with pytest.raises(ValueError):
+            config.parse_acl_table_info("TEST", "L3", None, "Ethernet200", "egress")
+
+    def test_parse_table_with_empty_ports(self):
+        with pytest.raises(ValueError):
+            config.parse_acl_table_info("TEST", "L3", None, "", "egress")
+
+    def test_acl_add_table_nonexistent_port(self):
+        runner = CliRunner()
+
+        result = runner.invoke(
+            config.config.commands["acl"].commands["add"].commands["table"],
+            ["TEST", "L3", "-p", "Ethernet200"])
+
+        assert result.exit_code != 0
+        assert "Cannot bind ACL to specified port Ethernet200" in result.output
+
+    def test_acl_add_table_empty_string_port_list(self):
+        runner = CliRunner()
+
+        result = runner.invoke(
+            config.config.commands["acl"].commands["add"].commands["table"],
+            ["TEST", "L3", "-p", ""])
+
+        assert result.exit_code != 0
+        assert "Cannot bind empty list of ports" in result.output
+
+    def test_acl_add_table_empty_vlan(self):
+        runner = CliRunner()
+
+        result = runner.invoke(
+            config.config.commands["acl"].commands["add"].commands["table"],
+            ["TEST", "L3", "-p", "Vlan3000"])
+
+        assert result.exit_code != 0
+        assert "Cannot bind empty VLAN Vlan3000" in result.output
