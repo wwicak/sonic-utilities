7fe50faafa2cd6fe44187625000475c6f721ada8
Vaibhav Hemant Dixit <vaibhav.dixit@microsoft.com>
2023-07-19 23:48:54 -0700

    UT change: for db_migrator test do not check for RESTAPI cert values (#2919)
    What I did
    MSFT ADO: 24598790
    
    Why:
    RESTAPI table has certs generated from minigraph parser.
    The values in RESTAPI table (cert and key names) are dependent on how they are hardcoded in minigraph parser.
    Current solution works on public repo, but breaks the internal build (different naming).
    
    What:
    Do not match values for RESTAPI attributes. The test still ensures that missing table and keys are migrated.
    
    How I did it
    How to verify it

--- tests/db_migrator_test.py
+++ tests/db_migrator_test.py
@@ -331,7 +331,13 @@ class TestWarmUpgrade_to_2_0_2(object):
         for table in new_tables:
             resulting_table = dbmgtr.configDB.get_table(table)
             expected_table = expected_db.cfgdb.get_table(table)
-            diff = DeepDiff(resulting_table, expected_table, ignore_order=True)
+            if table == "RESTAPI":
+                # for RESTAPI - just make sure if the new fields are added, and ignore values match
+                # values are ignored as minigraph parser is expected to generate different
+                # results for cert names based on the project specific config.
+                diff = set(resulting_table.get("certs").keys()) != set(expected_table.get("certs").keys())
+            else:
+                diff = DeepDiff(resulting_table, expected_table, ignore_order=True)
             assert not diff
 
     def test_warm_upgrade__without_mg_to_2_0_2(self):
