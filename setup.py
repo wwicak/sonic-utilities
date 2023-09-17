23f3ef9aa07f917b900beba54e4ec279aedefe8f
Saikrishna Arcot <sarcot@microsoft.com>
2023-09-17 13:27:34 -0700

    Allow newer versions of cryptography package
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- setup.py
+++ setup.py
@@ -227,7 +227,7 @@ setup(
     install_requires=[
         'bcrypt==3.2.2',
         'click==7.0',
-        'cryptography==3.3.2',
+        'cryptography>=3.3.2',
         'urllib3>=2',
         'click-log>=0.3.2',
         'docker>=4.4.4',
