063c64384ffefecefb61e6b94d5169931b916de5
Liu Shilong <shilongliu@microsoft.com>
2024-12-04 10:53:38 +0800

    reset to 5086

--- dump/plugins/__init__.py
+++ dump/plugins/__init__.py
@@ -1,22 +1,15 @@
 import os
+import sys
 import pkgutil
 import importlib
 from .executor import Executor
-from sonic_py_common.syslogger import SysLogger
 
 dump_modules = {}
 pkg_dir = os.path.dirname(__file__)
 
-log = SysLogger()
 # import child classes automatically
 for (module_loader, name, ispkg) in pkgutil.iter_modules([pkg_dir]):
-    try:
-        importlib.import_module('.' + name, __package__)
-    except ModuleNotFoundError as e:
-        if e.name != "dash_api":
-            # dash_api is only used in a specific platform
-            log.log_debug("dump utility - dash_api package not found for platform")
-            raise
+    importlib.import_module('.' + name, __package__)
 
 # Classes inheriting Executor
 dump_modules = {cls.__name__.lower(): cls for cls in Executor.__subclasses__()}
