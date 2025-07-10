58f250eb5e2136b54f3a4f54b738ec20eddbf926
Liping Xu <108326363+lipxu@users.noreply.github.com>
2025-07-10 09:08:06 +0800

    backport PR 3947 (#3960)
    backport PR 3947

--- clear/main.py
+++ clear/main.py
@@ -79,20 +79,20 @@ class AliasedGroup(click.Group):
 # location (configdb?), so that we prevent the continous execution of this
 # bash oneliner. To be revisited once routing-stack info is tracked somewhere.
 def get_routing_stack():
-    cmd0 = ["sudo", "docker", "ps"]
-    cmd1 = ["grep", "bgp"]
-    cmd2 = ["awk", '{print$2}']
-    cmd3 = ["cut", "-d-", "-f3"]
-    cmd4 = ["cut", "-d:", "-f1"]
+    result = 'frr'
+
+    cmd0 = ["sudo", "docker", "ps", "--format", "{{.Image}}\t{{.Names}}"]
+    cmd1 = ["awk", '$2 == "bgp"']
+    cmd2 = ["cut", "-d-", "-f3"]
+    cmd3 = ["cut", "-d:", "-f1"]
+    cmd4 = ["head", "-n", "1"]
 
     try:
         _, result = getstatusoutput_noshell_pipe(cmd0, cmd1, cmd2, cmd3, cmd4)
-
     except OSError as e:
         raise OSError("Cannot detect routing-stack")
 
     return (result)
-
 
 # Global Routing-Stack variable
 routing_stack = get_routing_stack()
