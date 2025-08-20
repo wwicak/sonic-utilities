b7011f534cdb609d89f61d24ec0b3436280c624b
mssonicbld <79238446+mssonicbld@users.noreply.github.com>
2025-08-20 08:13:23 +0800

    Switchport mode update for 'show interfaces status' (#4035)
    <!--
     Please make sure you've read and understood our contributing guidelines:
     https://github.com/Azure/SONiC/blob/gh-pages/CONTRIBUTING.md
    
     failure_prs.log skip_prs.log Make sure all your commits include a signature generated with `git commit -s` **
    
     If this is a bug fix, make sure your description includes "closes #xxxx",
     "fixes #xxxx" or "resolves #xxxx" so that GitHub automatically closes the related
     issue when the PR is merged.
    
     If you are adding/modifying/removing any command or utility script, please also
     make sure to add/modify/remove any unit tests from the tests
     directory as appropriate.
    
     If you are modifying or removing an existing 'show', 'config' or 'sonic-clear'
     subcommand, or you are adding a new subcommand, please make sure you also
     update the Command Line Reference Guide (doc/Command-Reference.md) to reflect
     your changes.
    
     Please provide the following information:
    -->
    
    #### What I did
    Fixed "show interfaces status" output for interfaces with switchport mode configuration.
    
    #### How I did it
    Configured "switchport mode" is fetched from DB for all front panel ports and PortChannel interfaces.
    "Vlan" column in "show interface status" is displayed based on below considerations:
    1. If interface is part of PortChannel - display PortChannel interface name [present behavior]
    2. If "switchport mode" is configured on interface [Ethernet/PortChannel] - display configured mode [new behavior]
    3. If "switchport mode" is NOT configured on interface but part of a VLAN [Ethernet/PortChannel] - display as "trunk" [present behavior]
    4. If "switchport mode" is NOT configured on interface and NOT part of a VLAN [Ethernet/PortChannel] - display as "routed" [present behavior]
    
    #### How to verify it
    Configure "switchport mode" for an interface/portchannel and verify the output in below show commands
    1. show interfaces switchport status
    2. show interfaces status
    
    #### Previous command output (if the output of a command-line utility has changed)
    Previously it was always displaying as `trunk` even if interface is configured as `access`
    ```
    root@sonic:~# config switchport mode access Ethernet0
    Ethernet0 switched to access mode
    
    root@sonic:~# show interfaces switchport status | grep Ethernet0
    Ethernet0 access
    
    root@sonic:~#
    root@sonic:~# show interfaces status | grep Ethernet0
     Ethernet0 2304,2305,2306,2307 100G 9100 N/A etp0 trunk up up QSFP28 or later N/A
    ```
    
    #### New command output (if the output of a command-line utility has changed)
    ```
    root@sonic:~# config switchport mode access Ethernet0
    Ethernet0 switched to access mode
    
    root@sonic:~# show interfaces switchport status | grep Ethernet0
    Ethernet0 access
    
    root@sonic:~#
    root@sonic:~# show interfaces status | grep Ethernet0
     Ethernet0 2304,2305,2306,2307 100G 9100 N/A etp0 access up up QSFP28 or later N/A
    ```
    
    closes #3787

--- tests/vlan_test.py
+++ tests/vlan_test.py
@@ -13,6 +13,10 @@ from utilities_common.db import Db
 from importlib import reload
 import utilities_common.bgp_util as bgp_util
 
+root_path = os.path.dirname(os.path.abspath(__file__))
+modules_path = os.path.dirname(root_path)
+scripts_path = os.path.join(modules_path, "scripts")
+
 IP_VERSION_PARAMS_MAP = {
     "ipv4": {
         "table": "VLAN"
@@ -296,12 +300,24 @@ test_config_add_del_with_switchport_modes_changes_output = """\
 """
 
 
+def get_intf_switchport_status(self, output: str, interface: str) -> str:
+    for line in output.splitlines():
+        line = line.strip()
+        if not line or line.startswith("Interface") or line.startswith("----"):
+            continue
+        parts = line.split()
+        if parts[0] == interface and len(parts) >= 2:
+            return parts[1]
+    return "interface not found"
+
+
 class TestVlan(object):
     _old_run_bgp_command = None
 
     @classmethod
     def setup_class(cls):
-        os.environ['UTILITIES_UNIT_TESTING'] = "1"
+        os.environ["PATH"] += os.pathsep + scripts_path
+        os.environ['UTILITIES_UNIT_TESTING'] = "2"
         # ensure that we are working with single asic config
         cls._old_run_bgp_command = bgp_util.run_bgp_command
         bgp_util.run_bgp_command = mock.MagicMock(
@@ -718,6 +734,9 @@ class TestVlan(object):
         print(result.exit_code)
         print(result.output)
         assert result.exit_code == 0
+        result = runner.invoke(show.cli.commands["interfaces"].commands["switchport"].commands["status"], obj=db)
+        switchport_status = get_intf_switchport_status(self, result.output, "PortChannel0001")
+        assert "routed" in switchport_status
 
         # Configure PortChannel0001 to routed mode again; should give error as it is already in routed mode
         result = runner.invoke(config.config.commands["switchport"].commands["mode"],
@@ -748,6 +767,10 @@ class TestVlan(object):
         print(result.exit_code)
         print(result.output)
         assert result.exit_code == 0
+        result = runner.invoke(show.cli.commands["interfaces"].commands["switchport"].commands["status"], obj=db)
+        print(result.output)
+        switchport_status = get_intf_switchport_status(self, result.output, "PortChannel1001")
+        assert "access" in switchport_status
 
         # Configure PortChannel1001 back to routed mode
         result = runner.invoke(config.config.commands["switchport"].commands["mode"],
@@ -755,6 +778,9 @@ class TestVlan(object):
         print(result.exit_code)
         print(result.output)
         assert result.exit_code == 0
+        result = runner.invoke(show.cli.commands["interfaces"].commands["switchport"].commands["status"], obj=db)
+        switchport_status = get_intf_switchport_status(self, result.output, "PortChannel1001")
+        assert "routed" in switchport_status
 
         # Configure PortChannel1001 to trunk mode
         result = runner.invoke(config.config.commands["switchport"].commands["mode"],
@@ -762,6 +788,9 @@ class TestVlan(object):
         print(result.exit_code)
         print(result.output)
         assert result.exit_code == 0
+        result = runner.invoke(show.cli.commands["interfaces"].commands["switchport"].commands["status"], obj=db)
+        switchport_status = get_intf_switchport_status(self, result.output, "PortChannel1001")
+        assert "trunk" in switchport_status
 
         # Add back PortChannel1001 tagged member to Vlan4000
         result = runner.invoke(config.config.commands["vlan"].commands["member"].commands["add"],
@@ -1176,6 +1205,10 @@ class TestVlan(object):
         print(result.output)
         assert result.exit_code == 0
         assert "Ethernet20 switched to access mode" in result.output
+        result = runner.invoke(show.cli.commands["interfaces"].commands["switchport"].commands["status"], obj=db)
+        print(result.output)
+        switchport_status = get_intf_switchport_status(self, result.output, "Ethernet20")
+        assert "access" in switchport_status
 
         # configure Ethernet20 to access mode again; should give error as it is already in access mode
         result = runner.invoke(config.config.commands["switchport"].commands["mode"], ["access", "Ethernet20"], obj=db)
@@ -1215,6 +1248,10 @@ class TestVlan(object):
         print(result.output)
         traceback.print_tb(result.exc_info[2])
         assert result.exit_code == 0
+        result = runner.invoke(show.cli.commands["interfaces"].commands["switchport"].commands["status"], obj=db)
+        print(result.output)
+        switchport_status = get_intf_switchport_status(self, result.output, "Ethernet20")
+        assert "trunk" in switchport_status
 
         # show output
         result = runner.invoke(show.cli.commands["vlan"].commands["brief"], [], obj=db)
@@ -1256,6 +1293,11 @@ class TestVlan(object):
         assert result.exit_code == 0
         assert "Ethernet20 switched to routed mode" in result.output
 
+        result = runner.invoke(show.cli.commands["interfaces"].commands["switchport"].commands["status"], obj=db)
+        print(result.output)
+        switchport_status = get_intf_switchport_status(self, result.output, "Ethernet20")
+        assert "routed" in switchport_status
+
         # del 1001
         result = runner.invoke(config.config.commands["vlan"].commands["del"], ["1001"], obj=db)
         print(result.exit_code)
@@ -1287,6 +1329,11 @@ class TestVlan(object):
         assert result.exit_code == 0
         assert "Ethernet20 switched to trunk mode" in result.output
 
+        result = runner.invoke(show.cli.commands["interfaces"].commands["switchport"].commands["status"], obj=db)
+        print(result.output)
+        switchport_status = get_intf_switchport_status(self, result.output, "Ethernet20")
+        assert "trunk" in switchport_status
+
         # add Ethernet64 to vlan 1001 but Ethernet64 is in routed mode will give error
         result = runner.invoke(config.config.commands["vlan"].commands["member"].commands["add"],
                                ["1001", "Ethernet64"], obj=db)
@@ -1302,6 +1349,10 @@ class TestVlan(object):
         print(result.output)
         assert result.exit_code == 0
         assert "Ethernet64 switched to trunk mode" in result.output
+        result = runner.invoke(show.cli.commands["interfaces"].commands["switchport"].commands["status"], obj=db)
+        print(result.output)
+        switchport_status = get_intf_switchport_status(self, result.output, "Ethernet20")
+        assert "trunk" in switchport_status
 
         # add Ethernet64 to vlan 1001
         result = runner.invoke(config.config.commands["vlan"].commands["member"].commands["add"],
@@ -1331,6 +1382,11 @@ class TestVlan(object):
         print(result.output)
         assert result.exit_code == 0
         assert "Ethernet64 switched to access mode" in result.output
+
+        result = runner.invoke(show.cli.commands["interfaces"].commands["switchport"].commands["status"], obj=db)
+        print(result.output)
+        switchport_status = get_intf_switchport_status(self, result.output, "Ethernet64")
+        assert "access" in switchport_status
 
         # show output
         result = runner.invoke(show.cli.commands["vlan"].commands["brief"], [], obj=db)
